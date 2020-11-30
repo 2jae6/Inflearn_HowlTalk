7de1cf26f85d1a51426fe799d24721f426112faf
JaeWook Lee <2jae66@gmail.com>
2020-11-30 23:25:46 +0900

    pod install

--- Inflearn_HowlTalk/Inflearn_HowlTalk/ViewController.swift
+++ Inflearn_HowlTalk/Inflearn_HowlTalk/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    
+    var box = UIImageView()
+    var remoteConfig: RemoteConfig!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+  
+        
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+
+        
+        
+        
+        self.view.addSubview(box)
+        box.snp.makeConstraints{ (make) in
+            make.center.equalTo(self.view)
+        }
+        box.image = UIImage(named: "loading_icon")
+        self.view.backgroundColor = UIColor(hex: "000000")
     }
 
 
+}
+ 
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
 }
 
